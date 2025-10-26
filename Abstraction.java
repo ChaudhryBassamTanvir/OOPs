633853f31e12dd7d3505536e808d3913b321a771
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-26 21:28:47 +0500

    updated

---
+++ Abstraction.java
@@ -0,0 +1,22 @@
+abstract class Animal{
+    public void walk(){
+//no need to show this to user
+    }
+}
+class Horse extends Animal{
+    public void walk(){
+        System.out.println("Walks on 4 legs");
+    }
+}
+
+class Chicken extends Animal{
+    public void walk(){
+        System.out.println("Walks on two legs");
+    }
+}
+public class Abstraction {
+    //Mean important things to show user and unncessary one hide
+
+
+
+}
--- Inheritance.java
+++ Inheritance.java
@@ -1,14 +1,52 @@
+
+import java.util.*;
+
+import bank.*;
+
+
 class Shape{
-    String color;
+public void area(){
+    System.out.println("Display area");
+}
+
 
 }
 class Triangle extends Shape{
-
+ public void area(int l, int h){
+    System.out.println(1/2*l*h);
+ }
 }
+
+class EquileteralTraingle extends Triangle{
+    public void area(int l, int h){
+    System.out.println(1/2*l*h);
+    }
+}
+
+
+
+class Circle extends Shape{
+    public void area(int r){
+        System.out.println((3.14)*r*r);
+    }
+}
+
+
+
+
 
 public class Inheritance {
     public static void main(String[] args) {
         Triangle t1= new Triangle();
-        t1.color="Blue";
+    bank.Account account1 = new bank.Account();
+    account1.name = "Customer1";
     }
 }
+
+
+//4 types of inheritance in java
+//Single level
+//mutilevel
+//Hierarchial Inheritance
+//Hybrid Inheritance (all inheritance)
+//Multiple inheritance in java is Interface
