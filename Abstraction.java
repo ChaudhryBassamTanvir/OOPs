6ee49c9f31adc7abe4b6d20f0f7f9c5792a9d1f2
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-10-26 21:33:23 +0500

    updated

--- Abstraction.java
+++ Abstraction.java
@@ -1,9 +1,20 @@
 abstract class Animal{
+        Animal(){
+        System.out.println("You created an animal");
+    }
    abstract public void walk();
+   public void eat(){
+    System.out.println("Animal eats");
+
+   }
 //no need to show this to user
     
 }
 class Horse extends Animal{
+Horse(){
+    System.out.println("You created a Horse");
+}
+
     public void walk(){
         System.out.println("Walks on 4 legs");
     }
@@ -16,7 +27,11 @@ class Chicken extends Animal{
 }
 public class Abstraction {
     //Mean important things to show user and unncessary one hide
-
+public static void main(String[] args) {
+    Horse horse = new Horse();
+    horse.walk();
+    horse.eat();
+}
 
 
 }
